12e61c0fe992e6552457a0e11dd2ed4d330b39b1
Thomas Hartka <thartka@Thomass-MacBook-Pro-2.local>
2016-01-11 14:31:35 -0500

    Added init files

--- CoolButton/ViewController.swift
+++ CoolButton/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Red_image: UIImageView!
+    @IBOutlet var Blue_image: UIImageView!
+    @IBOutlet var Blue_button: UIButton!
+    @IBOutlet var Red_button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+         Red_button.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Press_blue_button(sender: AnyObject) {
+        Red_image.hidden = true
+        Blue_image.hidden = false
+        Red_button.hidden = false
+        Blue_button.hidden = true
+    }
+    @IBAction func Press_red_button(sender: AnyObject) {
+        Red_image.hidden = false
+        Blue_image.hidden = true
+        Red_button.hidden = true
+        Blue_button.hidden = false
+    }
 }
 
